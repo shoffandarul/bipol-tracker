476355f033f6e6412b46192f1088b952f444c0f9
ezamusyarof <ezamusyarof06@gmail.com>
2022-12-29 11:24:23 +0700

    update database

---
+++ bipol_tracker.sql
@@ -0,0 +1,133 @@
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: bipol_tracker
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bipol`
+--
+
+DROP TABLE IF EXISTS `bipol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bipol` (
+  `id_bipol` int(11) NOT NULL AUTO_INCREMENT,
+  `plat_nomor` varchar(12) NOT NULL,
+  `id_driver` int(11) NOT NULL,
+  PRIMARY KEY (`id_bipol`),
+  UNIQUE KEY `plat_nomor` (`plat_nomor`),
+  CONSTRAINT `bipol_ibfk_1` FOREIGN KEY (`id_driver`) REFERENCES `driver` (`id_driver`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bipol`
+--
+
+LOCK TABLES `bipol` WRITE;
+/*!40000 ALTER TABLE `bipol` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bipol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `driver`
+--
+
+DROP TABLE IF EXISTS `driver`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `driver` (
+  `id_driver` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` text NOT NULL,
+  `username` varchar(24) NOT NULL,
+  `password` varchar(24) NOT NULL,
+  PRIMARY KEY (`id_driver`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `driver`
+--
+
+LOCK TABLES `driver` WRITE;
+/*!40000 ALTER TABLE `driver` DISABLE KEYS */;
+/*!40000 ALTER TABLE `driver` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jadwal`
+--
+
+DROP TABLE IF EXISTS `jadwal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jadwal` (
+  `id_jadwal` int(11) NOT NULL AUTO_INCREMENT,
+  `id_bipol` int(11) NOT NULL,
+  `hari` varchar(6) NOT NULL,
+  `waktu` time NOT NULL,
+  `halte` text NOT NULL,
+  PRIMARY KEY (`id_jadwal`),
+  KEY `id_bipol` (`id_bipol`),
+  CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_bipol`) REFERENCES `bipol` (`id_bipol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jadwal`
+--
+
+LOCK TABLES `jadwal` WRITE;
+/*!40000 ALTER TABLE `jadwal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jadwal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posisi`
+--
+
+DROP TABLE IF EXISTS `posisi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posisi` (
+  `id_bipol` int(11) NOT NULL,
+  `posisi` text NOT NULL,
+  `waktu` time NOT NULL,
+  `kapasitas` varchar(8) NOT NULL,
+  PRIMARY KEY (`id_bipol`),
+  CONSTRAINT `posisi_ibfk_1` FOREIGN KEY (`id_bipol`) REFERENCES `bipol` (`id_bipol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posisi`
+--
+
+LOCK TABLES `posisi` WRITE;
+/*!40000 ALTER TABLE `posisi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posisi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-29 11:20:08
